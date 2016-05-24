01eda8a1eb133ba02a0b3cd086e56e78559f42a0
Liam Westman <liamwest416@@ed.amdsb.ca>
2016-05-24 10:10:45 -0400

    Editing format for options window! :) ~ Liam

--- Character creator/Screens/OptionsScreen.Designer.cs
+++ Character creator/Screens/OptionsScreen.Designer.cs
@@ -28,10 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OptionsScreen));
+            this.optionsLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // optionsLabel
+            // 
+            this.optionsLabel.AutoSize = true;
+            this.optionsLabel.BackColor = System.Drawing.Color.Transparent;
+            this.optionsLabel.Font = new System.Drawing.Font("Agency FB", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.optionsLabel.ForeColor = System.Drawing.Color.White;
+            this.optionsLabel.Location = new System.Drawing.Point(269, 89);
+            this.optionsLabel.Name = "optionsLabel";
+            this.optionsLabel.Size = new System.Drawing.Size(156, 58);
+            this.optionsLabel.TabIndex = 0;
+            this.optionsLabel.Text = "OPTIONS";
+            // 
+            // OptionsScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.Controls.Add(this.optionsLabel);
+            this.Name = "OptionsScreen";
+            this.Size = new System.Drawing.Size(700, 500);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label optionsLabel;
     }
 }
