9ad9ca148587371a6a3ce16183ce28684e830dc7
Zevshoa <zshoag@gmail.com>
2016-06-07 10:09:18 -0400

    Worked on battlescreen

--- Character creator/Screens/BattleScreen.cs
+++ Character creator/Screens/BattleScreen.cs
@@ -12,15 +12,59 @@ namespace Character_creator
 {
     public partial class BattleScreen : UserControl
     {
+        Random ranNum = new Random();
+        int attackNum;
+
         public BattleScreen()
         {
             InitializeComponent();
         }
-
+        string attack1, attack2,attack3;
         private void BattleScreen_Load(object sender, EventArgs e)
         {
-            characterBox.Image = weaponScreen.finalPic;
-            
+            attackNum = 
+            characterBox.Image = reviewScreen.ch.picture;
+             switch (reviewScreen.ch.weapon)
+            {
+                case "Axe":
+                    attack1 = "Heavy Swing";
+                    break;
+                case "Sword":
+                    attack1 = "Slash";
+                    break;
+                case "Dagger":
+                    attack1 = "Knife Throw";
+                    break;
+                case "Staff":
+                    attack1 = "Fireball";
+                    break;
+                case "Fist":
+                    attack1 = "Sucker punch";
+                    break;
+                default:
+                    break;
+            }
+            switch(reviewScreen.ch.clothes)
+            {
+                case "Knight":
+                    attack2 = "Healing";
+                    break;
+                case "Deprived":
+                    attack2 = "Stink attack";
+                    break;
+                case "Thief":
+                    attack2 = "BackStab";
+                    break;
+                case "Mage":
+                    attack2 = "Regeneration";
+                    break;
+                case "Peasant":
+                    attack2 = "Economic Crisis";
+                    break;
+                default:
+                    break;
+            }
+
         }
     }
 }
