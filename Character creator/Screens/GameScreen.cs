1bd62749d52c0f4c2d59cf1bca958e3d7b2360f8
Liam Westman <liamwest416@@ed.amdsb.ca>
2016-06-13 08:46:14 -0400

    focused

--- Character creator/Screens/GameScreen.cs
+++ Character creator/Screens/GameScreen.cs
@@ -30,7 +30,7 @@ namespace Character_creator
         private void GameScreen_Load(object sender, EventArgs e)
         {
             //characterBox.Image = weaponScreen.finalPic;
-           // this.Focus();
+            this.Focus();
         }
 
         private void exitButton_Click(object sender, EventArgs e)
