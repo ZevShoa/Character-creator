d22530e152c8c9fdd2221b5f96ffee3544c8b074
Liam Westman <liamwest416@ed.amdsb.ca>
2016-06-14 19:48:00 -0400

    Leaderboard stuff

---
+++ Character creator/Highscore.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Character_creator
+{
+    class Highscore
+    {
+        public string name;
+        public int score; 
+        public Highscore(int _score, string _name)
+        {
+            score = _score;
+            name = _name;
+        }
+    }
+}
