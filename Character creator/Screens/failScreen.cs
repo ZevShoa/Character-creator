d94dbf22db88953e94d8677ec17603d9b88acfcb
Zevshoa <zshoag@gmail.com>
2016-06-20 10:08:37 -0400

    fixing xml stuff

--- Character creator/Screens/failScreen.cs
+++ Character creator/Screens/failScreen.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace Character_creator
 {
@@ -25,8 +26,33 @@ namespace Character_creator
 
         private void button1_Click(object sender, EventArgs e)
         {
+            saveXML();
             //closes the program if player rage quits
             Application.Exit();
+        }
+
+        private static void saveXML()
+        {
+            XmlTextWriter writer = new XmlTextWriter("Characters.xml", null);
+            writer.WriteStartElement("Characters");
+            for (int i = 0; i < characterDB.Count(); i++)
+            {
+                //Start "Employee" element
+                writer.WriteStartElement("Characters");
+
+
+                writer.WriteElementString("charClass", characterDB[i].charClass);
+                writer.WriteElementString("Name", characterDB[i].name);
+                writer.WriteElementString("Dex", characterDB[i].dexterity);
+                writer.WriteElementString("Strength", characterDB[i].strength);
+                writer.WriteElementString("Health", characterDB[i].health);
+                writer.WriteElementString("Perk", characterDB[i].perk);
+
+                // end the "Employee" element
+                writer.WriteEndElement();
+            }
+            //Write the XML to file and close the writer
+            writer.Close();
         }
 
         private void menuButton_Click(object sender, EventArgs e)
